b041b4d004563a394a2f247dbd62161fdb4f68c4
Roman Zhylko <roman.zhilko@gmail.com>
2020-12-15 18:02:25 +0300

    Test fix

--- Lab7/src/test/java/AddToCart.java
+++ Lab7/src/test/java/AddToCart.java
@@ -1,11 +1,13 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,17 +15,17 @@ public class AddToCart {
     private WebDriver driver;
     private WebElement addCartButton;
 
-    @BeforeClass
+    @Before
     public void setUpDriverAndClickFavoritesButton() {
         driver = new FirefoxDriver();
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(20, TimeUnit.SECONDS);
         driver.get("https://oz.by/pens/more10558337.html");
         addCartButton = driver.findElement(By.xpath("//*[@id=\"top-page\"]/div[3]/div/div[1]/div/div[2]/div[3]/div/div[1]/div[2]/div/div[1]/form/button/span[1]/span[2]"));
         addCartButton.click();
     }
 
-    @AfterClass
+    @After
     public void closeBrowser(){
         driver.quit();
         driver = null;
@@ -32,7 +34,7 @@ public class AddToCart {
 
     @Test
     public void favoritesButtonTextChangedTest() {
-        WebElement addCartButtonChanged = (new WebDriverWait(driver, 10))
+        WebElement addCartButtonChanged = (new WebDriverWait(driver, 20))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"top-page\"]/div[3]/div/div[1]/div/div[2]/div[3]/div/div[1]/div[2]/div/div[1]/a/span/span[2]")));
         String expectedTextButton = "Уже в корзине";
         String actualTextButton = addCartButtonChanged.getText();
@@ -41,7 +43,7 @@ public class AddToCart {
 
     @Test
     public void favoritesButtonTextColorChangedTest(){
-        WebElement addCartButtonChanged = (new WebDriverWait(driver, 10))
+        WebElement addCartButtonChanged = (new WebDriverWait(driver, 20))
             .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"top-page\"]/div[3]/div/div[1]/div/div[2]/div[3]/div/div[1]/div[2]/div/div[1]/a/span/span[2]")));
         String expectedColor = "rgb(205, 69, 0)";
         String actualColor = addCartButtonChanged.getCssValue("color");
@@ -50,7 +52,7 @@ public class AddToCart {
 
     @Test
     public void favoritesCounterChangedTest(){
-        WebElement favoritesCounter = (new WebDriverWait(driver, 10))
+        WebElement favoritesCounter = (new WebDriverWait(driver, 20))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"top-page\"]/div[1]/div[2]/div/ul/li[3]/a/span")));
         String expectedCount = "1";
         String actualCount = favoritesCounter.getText();
